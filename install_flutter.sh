c423475d429a5e7d910602589c653ef104742ca0
totenkopf7 <kurdishlearner2017@gmail.com>
2025-05-06 04:56:59 +0300

    Fix: add logging and PATH in install script

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-# Install Flutter SDK
-git clone https://github.com/flutter/flutter.git -b stable
-export PATH="$PATH:$PWD/flutter/bin"
+set -e  # Fail on first error
 
-# Enable web support
+echo "Cloning Flutter..."
+git clone https://github.com/flutter/flutter.git -b stable
+
+echo "Adding Flutter to PATH..."
+export PATH="$PWD/flutter/bin:$PATH"
+
+echo "Running flutter doctor..."
+flutter doctor
+
+echo "Enabling web support..."
 flutter config --enable-web
 
-# Pre-download dependencies
+echo "Pre-caching dependencies..."
 flutter precache
+
+echo "Running pub get..."
 flutter pub get
+
+echo "Building web app..."
+flutter build web
