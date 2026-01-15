6eee1ec3d15bdcbc27936fe27f4c97cd9700bccd
totenkopf7 <kurdishlearner2018@gmail.com>
2026-01-16 00:46:54 +0300

    Created appColors file

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // static const primaryColor = Color(0xFF2E7D32);
-  static const primaryColor = Color.fromARGB(255, 0, 0, 0);
+  static const primaryColor = Color(0xFF2E7D32);
   static const secondaryColor = Color(0xFF63CFF1);
   static const accentColor = Color(0xFF4CAF50);
   static const neutralColor = Color(0xFFCDD3D9);
